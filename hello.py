5e3c87f5cf159e34be24ea3233a3861a3abf25f9
Nohyunwoo <zz47911@naver.com>
2024-06-25 18:33:02 +0900

    this test-2

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ def profile(name, age, *language):
     for lang in language:
         print(lang)
     print()
+    print("보이나요")
 
-
+    print("안보이나요?")
 profile("노현우", 25, "파이썬", "자바", "C", "C++", "C#")
 profile("박기훈", 24, "코틀린", "스위프트","", "", "")
+p
