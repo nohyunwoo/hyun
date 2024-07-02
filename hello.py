83ef6e6da851581b8b7b2c93bb34d3ca83966d43
Nohyunwoo <zz47911@naver.com>
2024-07-02 14:36:05 +0900

    game

--- hello.py
+++ hello.py
@@ -1,12 +1,28 @@
 
-def profile(name, age, *language):
-    print("이름: {0} 나이: {1}\t".format(name,age), end="")
-    for lang in language:
-        print(lang)
-    print()
-    print("보이나요")
+def attack(name, location, damage):
+    print("{0} : {1} 방향 적군을 공격합니다. [공격력 {2}]".format(name, location, damage))
 
-    print("안보이나요??????????????????????")
-    print("뭐가 문제일까?")
-profile("노현우", 25, "파이썬", "자바", "C", "C++", "C#")
-profile("박기훈", 24, "코틀린", "스위프트","", "", "")
+name = "보병"
+hp = 40
+damage = 5
+
+print("{} 유닛을 생성했습니다.".format(name))
+print("체력 {0}, 공격력 {1}\n".format(hp,damage))
+
+tank_name = "탱크"
+tank_hp = 150
+tank_damage = 35
+
+print("{} 유닛을 생성했습니다.".format(tank_name))
+print("체력 {0}, 공격력 {1}\n".format(tank_hp,tank_damage))
+
+tank2_name = "탱크"
+tank2_hp = 150
+tank2_damage = 35
+
+print("{} 유닛을 생성했습니다.".format(tank2_name))
+print("체력 {0}, 공격력 {1}\n".format(tank2_hp,tank2_damage))
+
+attack(name, "1시", damage)
+attack(tank_name, "11사", tank_damage)
+attack(tank_name, "11사", tank2_damage)
