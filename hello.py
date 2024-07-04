fc3b19f31ff3dbbf90b688755d9b438557f3df28
Nohyunwoo <zz47911@naver.com>
2024-07-04 11:50:18 +0900

    finish game

--- hello.py
+++ hello.py
@@ -1,12 +1,29 @@
+from random import *
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+    
+def game_over():
+    print("Player : Good Game")
+    print("[Plater] 님이 게임에서 퇴장했습니다.")
+
 class Unit:
     def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
         self.speed = speed
+        print("{0} 유닛을 생성했습니다.".format(name))
 
     def move(self, location):
-        print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
+
+    def damaged(self, damage):
+        print("{0} : {1}만큼 피해를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name)) 
+
 class AttackUnit(Unit):
     def __init__(self, name, hp, damage, speed):
         Unit. __init__(self, name, hp, speed)
@@ -14,13 +31,6 @@ class AttackUnit(Unit):
 
     def attack(self, location):
         print("{0} : {1} 방향 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
-
-    def damaged(self, damage):
-        print("{0} : {1}만큼 피해를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
 
 class Flyable:
     def __init__(self, flying_speed):
@@ -35,19 +45,93 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         Flyable. __init__(self, flying_speed)
     
     def move(self, location):
-        print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
-hoverbike = AttackUnit("호버 바이크", 80, 20, 10) # 지상유닛
-spaceruiser = FlyableAttackUnit("우주 순양함", 500, 25, 3)
-hoverbike.move("11시")
-spaceruiser.move("9시")
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(name, hp, 0)
+        super(). __init__(name, hp, 0)
+        self.location = location
 
-# intercetor = FlyableAttackUnit("요격기", 200, 6, 5)
-# intercetor.fly(intercetor.name, "3시")
-# intercetor.attack("3시")
+class Soldier(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self, "보병", 40, 5, 1)
 
-# flamethrower1 = AttackUnit("화염방사병", 50, 16)
-# flamethrower1.attack("3시")
-# flamethrower1.damaged(25)
-# flamethrower1.damaged(25)
+    def booster(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print("{0} : 강화제를 사용합니다. (HP 10 감소)".format(self.name))
+        else:
+            print("{0} : 체력이 부족해 기술을 사용할 수 없습니다.".format(self.name))
+
+class Tank(AttackUnit):
+    siege_developed = True
+    print("[알림] 탱크의 시즈 모드 개발이 완료되었습니다.")
+
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 35, 1)
+        self.siege_mode = False
+
+    def set_siege_mode(self):
+        if Tank.siege_developed == False:
+            return
+        if self.siege_mode == False:
+            print("{0} : 시즈 모드로 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.siege_mode = True
+        else:
+            print("{0} : 시즈 모드를 해제합니다.".format(self.name))
+            self.damage //=2
+            self.siege_mode = False
+
+class Stealth(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit. __init__(self, "전투기", 80, 20, 5)
+        self.cloaked = False
+
+    def cloacking(self):
+        if self.cloaked == True:
+            print("{0} : 은폐 모드를 해제합니다".format(self.name))
+            self.cloaked = False
+        else:
+            print("{0} : 은폐 모드를 설정합니다.".format(self.name))
+            self.cloaked == True
+
+
+game_start()
+
+so1 =Soldier()
+so2 =Soldier()
+so3 =Soldier()
+
+ta1 = Tank()
+ta2 = Tank()
+
+st1 = Stealth()
+
+attack_unit = []
+attack_unit.append(so1)
+attack_unit.append(so2)
+attack_unit.append(so3)
+attack_unit.append(ta1)
+attack_unit.append(ta2)
+attack_unit.append(st1)
+
+for unit in attack_unit:
+    unit.move("1시")
+
+for unit in attack_unit:
+    if isinstance(unit, Soldier):
+        unit.booster()
+    elif isinstance(unit, Tank):
+        unit.set_siege_mode()
+    elif isinstance(unit, Stealth):
+        unit.cloacking()
+
+for unit in attack_unit:
+    unit.attack("1시")
+
+for unit in attack_unit:
+    unit.damaged(randint(5,20))
+
+game_over()
